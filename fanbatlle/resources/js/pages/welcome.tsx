c55f0cd33e8c88c0c15c6e608c55008a7610d60e
Amineo21 <153727231+Amineo21@users.noreply.github.com>
2025-03-20 12:00:55 +0100

    feat:

--- fanbatlle/resources/js/pages/welcome.tsx
+++ fanbatlle/resources/js/pages/welcome.tsx
@@ -58,7 +58,7 @@ export default function Welcome() {
                             </div>
                         )}
                     </nav>
-                </header>
+                </div>
                 <div className="flex w-full items-center justify-center opacity-100 transition-opacity duration-750 lg:grow starting:opacity-0">
                     <main className="flex w-full max-w-[335px] flex-col-reverse lg:max-w-4xl lg:flex-row">
                         <div className="flex-1 rounded-br-lg rounded-bl-lg bg-white p-6 pb-12 text-[13px] leading-[20px] shadow-[inset_0px_0px_0px_1px_rgba(26,26,0,0.16)] lg:rounded-tl-lg lg:rounded-br-none lg:p-20 dark:bg-[#161615] dark:text-[#EDEDEC] dark:shadow-[inset_0px_0px_0px_1px_#fffaed2d]">
@@ -154,7 +154,7 @@ export default function Welcome() {
                         >
                             See more
                         </Link>
-                    </div>
+                    </main>
                 </div>
 
                 
