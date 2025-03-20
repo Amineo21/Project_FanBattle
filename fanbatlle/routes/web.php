066070d8e5d0f9b1fcab48e753722642b60fddf3
Mikael Lahlou <lahmikael@gmail.com>
2025-03-20 01:34:32 +0100

    🎖️frontend done + add routes and animation

--- fanbatlle/routes/web.php
+++ fanbatlle/routes/web.php
@@ -17,5 +17,9 @@ Route::get('/about', function () {
     return Inertia::render('about');
 })->name('about');
 
+Route::get('/udash', function () {
+    return Inertia::render('udash');
+})->name('udash');
+
 require __DIR__ . '/settings.php';
 require __DIR__ . '/auth.php';
